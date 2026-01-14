ddc01fc9fdf2f93b7b8ad1b44e7b705636b36dac
adi <218113568+Zenvok-arch@users.noreply.github.com>
2026-01-14 19:35:13 +0530

    seperated todolist from the main page and fix the id problem

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
+import Todolist from "./Todolist";
 
 const MainPage = () => {
     const [todo, setTodo] = useState(()=>{
@@ -32,7 +34,7 @@ const MainPage = () => {
         }
     else{
             setTodo(prev => [...prev, {
-            id: crypto.randomUUID(),
+            id: nanoid(),
             text: value,
             completed: false
         }])
@@ -107,31 +109,7 @@ const MainPage = () => {
 
             <div>
                 <h2 className='text-2xl font-medium pt-8 text-center  mb-4'>TASK LIST</h2>
-                <div>
-                    {todo.map((todo) => {
-                        return (<div key={todo.id} className='flex justify-between   text-xl font-medium gap-1.5 w-2/3  items-center border-amber-50 border p-2 mx-auto my-4'>
-                            <div className='flex w-full   gap-2'>
-                                <input type="checkbox"
-                                checked={todo.completed}
-                                onChange={()=>handelChecked(todo.id)}
-                                className='p-2 checkbox' 
-                                 />
-                                <p className={todo.completed ? "line-through opacity-60" : ""}
-                                >{todo.text}</p>
-                            </div>
-                            <div className='flex gap-2'>
-                                <button
-                                    onClick={() => handelDelete(todo.id)}
-                                    className='bg-[#6633eebf] py-1 px-2  font-medium'>Delete</button>
-                                { !todo.completed && <button
-                                    onClick={() => handelEdit(todo.id)}
-                                    className='bg-[#6633eebf] py-1 px-2 font-medium'>Edit</button>}
-                            </div>
-                        </div>)
-                    })}
-
-
-                </div>
+                <Todolist todo={todo} handelChecked={handelChecked} handelEdit={handelEdit} handelDelete={handelDelete} />
             </div>
 
 
---
+++ src/pages/Todolist.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Todolist = ({todo,handelChecked,handelDelete,handelEdit}) => {
+  return (
+<div>
+                    {todo.map((todo) => {
+                        return (<div key={todo.id} className=' Todolist flex justify-between   text-xl font-medium gap-1.5 w-2/3  items-center border-amber-50 border p-2 mx-auto my-4'>
+                            <div className='flex w-full   gap-2'>
+                                <input type="checkbox"
+                                checked={todo.completed}
+                                onChange={()=>handelChecked(todo.id)}
+                                className='p-2 checkbox' 
+                                 />
+                                <p className={todo.completed ? "line-through opacity-60" : ""}
+                                >{todo.text}</p>
+                            </div>
+                            <div className='flex gap-2'>
+                                <button
+                                    onClick={() => handelDelete(todo.id)}
+                                    className='bg-[#6633eebf] py-1 px-2  font-medium'>Delete</button>
+                                { !todo.completed && <button
+                                    onClick={() => handelEdit(todo.id)}
+                                    className='bg-[#6633eebf] py-1 px-2 font-medium'>Edit</button>}
+                            </div>
+                        </div>)
+                    })}
+
+
+                </div>
+  )
+}
+
+export default Todolist
