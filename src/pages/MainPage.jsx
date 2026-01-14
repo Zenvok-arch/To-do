1a30d1b4705c79087d8b2fa14c0b5e1190e626d5
adi <218113568+Zenvok-arch@users.noreply.github.com>
2026-01-14 18:58:54 +0530

    Revert "Seperated todolist from main page"
    This reverts commit abad6a68369c56b0882d242c03fdddab355664ae.

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import TodoList from "./TodoList";
 
 const MainPage = () => {
     const [todo, setTodo] = useState(()=>{
@@ -108,7 +107,31 @@ const MainPage = () => {
 
             <div>
                 <h2 className='text-2xl font-medium pt-8 text-center  mb-4'>TASK LIST</h2>
-                 <TodoList todo={todo} handelChecked={handelChecked} handelDelete={handelDelete} handelEdit={handelEdit}/>
+                <div>
+                    {todo.map((todo) => {
+                        return (<div key={todo.id} className='flex justify-between   text-xl font-medium gap-1.5 w-2/3  items-center border-amber-50 border p-2 mx-auto my-4'>
+                            <div className='flex w-full   gap-2'>
+                                <input type="checkbox"
+                                checked={todo.completed}
+                                onChange={()=>handelChecked(todo.id)}
+                                className='p-2 checkbox' 
+                                 />
+                                <p className={todo.completed ? "line-through opacity-60" : ""}
+                                >{todo.text}</p>
+                            </div>
+                            <div className='flex gap-2'>
+                                <button
+                                    onClick={() => handelDelete(todo.id)}
+                                    className='bg-[#6633eebf] py-1 px-2  font-medium'>Delete</button>
+                                { !todo.completed && <button
+                                    onClick={() => handelEdit(todo.id)}
+                                    className='bg-[#6633eebf] py-1 px-2 font-medium'>Edit</button>}
+                            </div>
+                        </div>)
+                    })}
+
+
+                </div>
             </div>
 
 
